e2332f2409861ffbea40edbfce408b397f573020
Abhishek <abhishek54162@gmail.com>
2025-09-22 21:32:05 +0530

    final

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://cerulean-bublanina-12d295.netlify.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
