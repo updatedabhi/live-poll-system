96e0435f61310e0d9cfd767039dfe49d992c5ce6
Abhishek <abhishek54162@gmail.com>
2025-09-22 19:53:00 +0530

    final

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://cerulean-bublanina-12d295.netlify.app/",
     credentials: true,
   })
 );
